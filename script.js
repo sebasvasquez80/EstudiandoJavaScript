9d1ad5023dbb5776efb099fc20f525b48362fac6
sebasvasquez80 <sebasvb20@gmail.com>
2024-10-23 21:59:58 -0500

    Se realizan ejercicios como ejercicio de estudio para parcial

---
+++ script.js
@@ -0,0 +1,60 @@
+// var numero = 2
+
+// if(numero == 1){
+//     console.log('Es igual')
+// }else{
+//     console.log('No es igual')
+// }
+
+// var numero = Number(prompt('Ingrese un numero'))
+
+// switch (numero) {
+//     case 1:
+//         console.log('Eres el numero uno')
+//         break;
+//     case 2:
+//         console.log('Eres el numero dos')
+//         break;
+//     case 3:
+//         console.log('Eres el numero tres')
+//         break;
+//     case 4:
+//         console.log('Eres el numero cuatro')
+//         break;
+//     default:
+//         alert('No esta ese numero')
+// }
+
+// var contador = 0
+
+// while(contador <= 10){
+//     console.log ('Ronda N°' + contador)
+//     contador+=2
+// }
+
+// for (var i=1; i < 20; i+=5){
+//     console.log(i);
+// }
+
+// var contador = 0;
+
+// do {
+//     console.log('Haz guardado ' + contador);
+//     contador += 50;
+// }
+// while (contador <= 100);
+
+// function saludar(){
+//     console.log('Hola mundo');
+// }
+
+// saludar();
+
+// function sumar(numeroUno,numeroDos){
+//     let resultado;
+//     resultado = numeroDos + numeroUno;
+//     return resultado;
+// }
+
+// let suma = sumar(5,9);
+// console.log(suma)
